cf18be6a4edbba026cd4379cfdee2d0b0d4a05be
Dmitriy745 <dmitriykukorov@gmail.com>
2025-09-26 11:57:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
+import pandas as pd
 import streamlit as st
+from numpy.random import default_rng as rng
+
+
+
+
+df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+st.line_chart(df)
+
+
 
 age = st.slider("How old are you?", 0, 130, 25)
 st.write("I'm ", age, "years old")
