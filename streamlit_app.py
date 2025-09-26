ba7f38dfc2c80fb19d45a1908a7ac2426d8adef9
Dmitriy745 <dmitriykukorov@gmail.com>
2025-09-26 11:40:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ age = st.slider("How old are you?", 0, 130, 25)
 st.write("I'm ", age, "years old")
 
 st.sidebar.text_input(age) 
+
+
+option = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
+)
